c26275464221a6622320776bcdaac8d3da1adf05
7kayoh <nakogls@outlook.com>
2022-01-22 15:11:14 +0900

    Punctuation: change Lets to Let's

--- src/views/signup.vue
+++ src/views/signup.vue
@@ -8,7 +8,7 @@
           <v-stepper-items>
             <v-stepper-content class="mx-n2 mt-n6" step="1">
               <v-card-text class="mt-n5 ml-n4 mt-n2 mt-n2 grey--text">
-                Lets start with your login info
+                Let's start with your login info
               </v-card-text>
               <v-alert type="error" v-if="error" class="mt-n2" color="red">
                 Please enter a valid username</v-alert
